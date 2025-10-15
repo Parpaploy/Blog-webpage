2d1e3876e6f167b6772d75ad971cd4e342fd299a
parpaploy <ramita1846@gmail.com>
2025-10-15 11:06:28 +0700

    add:reset-search-btn

--- src/components/navbar/components/search.tsx
+++ src/components/navbar/components/search.tsx
@@ -6,6 +6,7 @@ import { GoSearch } from "react-icons/go";
 import { ICategory } from "../../../../interfaces/strapi.interface";
 import { useRouter, useSearchParams } from "next/navigation";
 import CategoryMenu from "./category-menu";
+import { RiResetRightLine } from "react-icons/ri";
 
 function Search({
   isOpenCat,
@@ -24,14 +25,22 @@ function Search({
 
   const params = useSearchParams();
 
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState(params.get("query") || "");
+  const [canHover, setCanHover] = useState(true);
+
+  const updateSearchParams = (newQuery?: string, newCategories?: string[]) => {
+    const search = new URLSearchParams();
+    const finalQuery = newQuery ?? params.get("query") ?? "";
+    const finalCategories = newCategories ?? params.getAll("category");
+
+    if (finalQuery.trim() !== "") search.set("query", finalQuery);
+    finalCategories.forEach((c) => search.append("category", c));
+
+    router.push(`/search?${search.toString()}`);
+  };
 
   const handleSearch = () => {
-    if (query.trim() !== "") {
-      router.push(`/search?query=${encodeURIComponent(query)}`);
-    } else {
-      router.push(`/search`);
-    }
+    updateSearchParams(query);
   };
 
   const handleCategoryClick = (cat: string) => {
@@ -40,15 +49,12 @@ function Search({
       ? currentCategories.filter((c) => c !== cat)
       : [...currentCategories, cat];
 
-    const query = params.get("query");
-    const search = new URLSearchParams();
+    updateSearchParams(query, newCategories);
+  };
 
-    if (query) search.set("query", query);
-
-    newCategories.forEach((c) => search.append("category", c));
-
-    router.push(`/search?${search.toString()}`);
-    // setIsOpenCat(false);
+  const handleReset = () => {
+    setQuery("");
+    router.push(`/search`);
   };
 
   return (
@@ -59,10 +65,11 @@ function Search({
           placeholder="Search..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          className="w-full h-full bg-white/10 backdrop-blur-sm border border-white/30 shadow-lg rounded-4xl px-4 py-1"
+          className="w-full h-full bg-white/10 backdrop-blur-sm border border-white/30 shadow-lg rounded-4xl px-4 py-1 focus:ring-2 focus:ring-white/30 focus:outline-none"
           onKeyDown={(e) => e.key === "Enter" && handleSearch()}
         />
       </div>
+
       <div
         className="flex w-12 items-center justify-center h-full transition-all bg-white/10 hover:bg-white/30 backdrop-blur-sm border border-white/30 shadow-lg rounded-4xl px-2 py-1 cursor-pointer"
         onClick={handleSearch}
@@ -70,18 +77,40 @@ function Search({
         <GoSearch size={20} />
       </div>
 
+      <button
+        onClick={handleReset}
+        className="group flex w-10 items-center justify-center h-full transition-all bg-white/10 hover:bg-white/30 backdrop-blur-sm border border-white/30 shadow-lg rounded-4xl px-2 py-1 cursor-pointer"
+      >
+        <RiResetRightLine
+          size={20}
+          className="group-hover:rotate-360 transition-all duration-500"
+        />
+      </button>
+
       <div
-        className={`cursor-pointer ${
-          isOpenCat ? "hover:-translate-y-1" : "hover:translate-y-1"
-        } transition-all hover:text-white/90 text-center flex items-center justify-center text-white/80 absolute top-11 left-1/2 -translate-x-1/2`}
-        onClick={() => {
+        className={`cursor-pointer transition-all text-center flex items-center justify-center text-white/80 absolute top-11 left-1/2 -translate-x-1/2
+    ${
+      canHover
+        ? isOpenCat
+          ? "hover:-translate-y-1"
+          : "hover:translate-y-1"
+        : ""
+    }
+  `}
+        onClick={(e) => {
+          e.currentTarget.blur();
           setIsOpenCat(!isOpenCat);
+          setCanHover(false);
         }}
         onMouseEnter={() => {
-          setIsHover(true);
+          if (canHover) setIsHover(true);
         }}
         onMouseLeave={() => {
           setIsHover(false);
+          setCanHover(true);
+        }}
+        onMouseUp={() => {
+          setTimeout(() => setCanHover(false), 0);
         }}
       >
         {isOpenCat ? <IoIosArrowUp size={48} /> : <IoIosArrowDown size={48} />}
@@ -89,21 +118,23 @@ function Search({
 
       {isOpenCat && (
         <div
-          className={`w-full flex flex-wrap gap-3 items-center justify-center absolute top-25 left-1/2 -translate-x-1/2 transition-all ${
-            isHover && isOpenCat && "-translate-y-1 opacity-50"
-          }`}
+          className={`w-full flex flex-wrap gap-3 items-center justify-center absolute top-25 left-1/2 -translate-x-1/2 transition-all duration-300
+            ${
+              isHover && canHover
+                ? "-translate-y-1 opacity-50"
+                : "translate-y-0 opacity-100"
+            }
+          `}
         >
-          {categories.map((cat: ICategory, index: number) => {
-            return (
-              <CategoryMenu
-                key={index}
-                title={cat.title}
-                handleCategoryClick={handleCategoryClick}
-                catTitle={cat.title}
-                selectedCategories={params.getAll("category")}
-              />
-            );
-          })}
+          {categories.map((cat: ICategory, index: number) => (
+            <CategoryMenu
+              key={index}
+              title={cat.title}
+              handleCategoryClick={handleCategoryClick}
+              catTitle={cat.title}
+              selectedCategories={params.getAll("category")}
+            />
+          ))}
         </div>
       )}
     </div>
